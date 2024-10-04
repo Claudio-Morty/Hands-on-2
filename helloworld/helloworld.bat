b22d52d5df3c449c4ec46cdc793380dfc3766a0c
Claudio-Morty <claudio.ponce1346@alumnos.udg.mx>
2024-10-04 16:42:00 -0600

    Create helloworld.bat

---
+++ helloworld/helloworld.bat
@@ -0,0 +1,33 @@
+@echo off
+
+
+echo.
+
+
+echo Hello World
+
+
+echo.
+
+
+echo Listado del contenido del directorio actual:
+dir
+
+
+echo.
+
+
+mkdir Test
+
+
+cd Test
+
+
+echo.
+
+
+echo Listado del directorio Test
+dir
+
+
+pause
