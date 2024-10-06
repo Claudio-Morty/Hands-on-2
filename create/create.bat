defe3612d9e43952d869d182e1997145d9468964
Claudio-Morty <claudio.ponce1346@alumnos.udg.mx>
2024-10-06 00:41:30 -0600

    Update create.bat

--- create/create.bat
+++ create/create.bat
@@ -1,1 +1,61 @@
-b
+@echo off
+
+
+echo Se crea un archivo de texto plano llamado mytext.txt con la cadena "Hola Mundo"
+
+echo Hola Mundo > mytext.txt
+
+
+echo.
+
+
+echo Despliego el contenido del archivo mytext.txt:
+
+type mytext.txt
+
+
+echo.
+
+
+echo Se crea un subdirectorio llamado backup
+
+mkdir backup
+
+
+echo.
+
+
+echo Se copia el archivo mytext.txt al subdirectorio backup
+
+copy mytext.txt backup\
+
+
+echo.
+
+
+echo Listado de archivos en el subdirectorio backup:
+
+dir backup
+
+
+echo.
+
+
+echo Se elimina el archivo mytext.txt del subdirectorio backup
+
+del backup\mytext.txt
+
+
+echo.
+
+
+echo Se elimina el subdirectorio backup
+
+rmdir backup
+
+
+echo.
+
+
+pause
+
